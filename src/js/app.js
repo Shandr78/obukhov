3c0c4048a7353bddf5fd7b9ea5029fbed3ffb34b
Andrei Shulekin <A.Shulekin@ukr.net>
2023-03-21 11:59:09 +0200

    resp-2

--- src/js/app.js
+++ src/js/app.js
@@ -224,7 +224,7 @@ flsScroll.headerScroll();
 
 /* Динамічний адаптив */
 // Документація: https://template.fls.guru/template-docs/dinamicheskij-adaptiv.html
-// import "./libs/dynamic_adapt.js";
+import "./libs/dynamic_adapt.js";
 
 /* Форматування чисел */
 // import './libs/wNumb.min.js';
